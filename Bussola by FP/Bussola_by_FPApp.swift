fab2281599c23fa0b1db66e5f1f1b96cec9695d5
morbuen <morbuen@live.it>
2022-06-20 18:18:53 +0200

    Initial Commit

---
+++ Bussola by FP/Bussola_by_FPApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bussola_by_FPApp.swift
+//  Bussola by FP
+//
+//  Created by Francesco Puzone on 20/06/22.
+//
+
+import SwiftUI
+
+@main
+struct Bussola_by_FPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bussola by FP/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bussola by FP
+//
+//  Created by Francesco Puzone on 20/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
